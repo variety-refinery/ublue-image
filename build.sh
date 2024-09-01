3f069c3c513c361d7ee9a6d32ce772625d1d8203
micro <micro@noreply.localhost>
2024-09-01 13:13:38 -0500

    add most packages

--- build.sh
+++ build.sh
@@ -13,7 +13,43 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 curl --location --output /etc/yum.repos.d/tailscale.repo https://pkgs.tailscale.com/stable/fedora/tailscale.repo
-rpm-ostree install tailscale fish eza neovim
+
+# TODO: package and add todoman, reader, string-reminder, bat-extras, (new) micro-systemd-units, htmlq, prettier, stopmpd, topgrade, create-todo-user
+rpm-ostree install \
+	adw-gtk3-theme \
+	akregator \
+	bat \
+	chafa \
+	cryfs \
+	easyeffects \
+	eza \
+	fastfetch \
+	fish \
+	graphviz \
+	hyfetch \
+	inxi \
+	khal \
+	kleopatra \
+	krita \
+	merkuro \
+	mpv \
+	neovim \
+	nextcloud-client \
+	oxipng \
+	p7zip \
+	pnpm \
+	qalculate-gtk \
+	rustup \
+	skanpage \
+	speech-dispatcher \
+	starship \
+	tailscale \
+	tealdeer \
+	trash-cli \
+	vdirsyncer \
+	wine \
+	yq \
+	yt-dlp
 
 # this installs a package from fedora repos
 #rpm-ostree install screen
