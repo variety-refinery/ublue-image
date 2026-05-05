289f45dd59ff5a6cd9b7eb6d85ea4dcaf19ee4be
niko <niko@microspinny.zip>
2026-05-05 14:55:41 -0500

    add 8bitdo rules

--- build_files/build.sh
+++ build_files/build.sh
@@ -29,6 +29,7 @@ dnf5 install --assumeyes --setopt=tsflags=noscripts \
 
 cp /ctx/69-probe-rs.rules /etc/udev/rules.d
 cp /ctx/99-disable-framework-keyboard.rules /etc/udev/rules.d
+cp /ctx/71-8bitdo.rules /etc/udev/rules.d
 
 # packages
 dnf5 install --assumeyes \
